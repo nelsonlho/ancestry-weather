7eec3b6f7bce7424d2c51cca11e0a8446fbddadd
nelsonlho <nelsonlho@gmail.com>
2023-10-03 12:03:58 -0700

    post request working from front end, fixed cors issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,22 @@
 import express, { Request, Response } from 'express';
 import axios from 'axios';
+import cors from 'cors';
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/weather', async (req: Request, res: Response) => {
-  const location = 'Brisbane, CA';
-
+app.post('/weather', async (req: Request, res: Response) => {
+  const { body } = req;
+  const location = body.location;
+  if (location == null || location.trim() === '') {
+    return res
+      .status(400)
+      .json({ error: 'Invalid location. Please try again' });
+  }
   try {
     const apiKey = process.env.WEATHER_API_KEY as string;
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`;
