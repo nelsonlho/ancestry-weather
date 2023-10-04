e3311d301ff0cd471af32a617810ebb86e2d08cd
nelsonlho <nelsonlho@gmail.com>
2023-10-03 18:24:00 -0700

    set up autocomplete

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,11 +1,25 @@
-import { useState, ChangeEvent, KeyboardEvent, MouseEvent } from 'react';
+import { useState, useRef, ChangeEvent, KeyboardEvent, Ref } from 'react';
 import axios from 'axios';
+import { usePlacesWidget } from 'react-google-autocomplete';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL as string;
 const Search = () => {
   const [location, setLocation] = useState<string | null>(null);
+  const [coords, setCoords] = useState<{
+    lat: number | null;
+    lng: number | null;
+  }>({
+    lat: null,
+    lng: null,
+  });
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { ref } = usePlacesWidget<HTMLDivElement>({
+    apiKey: import.meta.env.VITE_MAP_API,
+    onPlaceSelected: (place) =>
+      console.log({ geometry: place.geometry.location.lng() }),
+  });
 
   const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
@@ -49,7 +63,7 @@ const Search = () => {
       <Input
         className="w-5/12 mx-2"
         onChange={(event) => onInputChange(event)}
-        onKeyDown={(event) => onKeyDown(event)}
+        ref={ref as React.RefObject<HTMLInputElement>}
       />
       <Button onClick={onButtonClick}>Search</Button>
     </div>
