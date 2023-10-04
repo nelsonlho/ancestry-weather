a4533ebd242d1f14437de48eea58000e25f9dbf5
nelsonlho <nelsonlho@gmail.com>
2023-10-03 23:10:36 -0700

    added loading state

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,11 +1,16 @@
-import { useEffect, useState, useRef, ChangeEvent, KeyboardEvent } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { usePlacesWidget } from 'react-google-autocomplete';
-import { Button } from '@/components/ui/button';
+import { Toaster } from '@/components/ui/toaster';
+import { ToastAction } from '@/components/ui/toast';
+import { useToast } from '@/components/ui/use-toast';
 import { Input } from '@/components/ui/input';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL as string;
 const Search = () => {
+  const { toast } = useToast();
+
+  const [loading, setLoading] = useState<boolean>(false);
   const [coords, setCoords] = useState<{
     lat: number | null;
     lng: number | null;
@@ -34,13 +39,25 @@ const Search = () => {
 
   const searchLocation = async () => {
     const { lat, lng } = coords;
-    const response = await axios.post(`${BASE_URL}/weather`, {
-      lat,
-      lng,
-    });
-    console.log({ response });
-    const data = await response;
-    console.log({ data });
+
+    try {
+      setLoading(true);
+      const response = await axios.post(`${BASE_URL}/weather`, {
+        lat,
+        lng,
+      });
+      const data = await response;
+      console.log({ loading, data });
+    } catch {
+      toast({
+        variant: 'destructive',
+        title: `There's a problem`,
+        description: 'Please try again later',
+        action: <ToastAction altText="Try again">Close</ToastAction>,
+      });
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -49,6 +66,8 @@ const Search = () => {
         className="w-5/12 mx-2"
         ref={ref as React.RefObject<HTMLInputElement>}
       />
+      <Toaster />
+      {loading ? <div>isLoading</div> : null}
     </div>
   );
 };
